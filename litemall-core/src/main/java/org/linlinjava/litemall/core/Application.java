31981dbe3d9306400689e8908f7103131dd18987
liuqingxiang <liuqingxiang@bestvike.com>
2019-04-17 21:19:34 +0800

    添加RestTemplateUtils类，封装http常用请求

--- litemall-core/src/main/java/org/linlinjava/litemall/core/Application.java
+++ litemall-core/src/main/java/org/linlinjava/litemall/core/Application.java
@@ -3,10 +3,17 @@ package org.linlinjava.litemall.core;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication(scanBasePackages = {"org.linlinjava.litemall.db", "org.linlinjava.litemall.core"})
 @MapperScan("org.linlinjava.litemall.db.dao")
 public class Application {
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
